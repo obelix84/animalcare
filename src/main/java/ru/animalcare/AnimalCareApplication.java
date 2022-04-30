8e9e83091b49e410c81633bf2d95a62530e81c21
obelix <sonicx921@yandex.ru>
2022-05-01 02:59:16 +0300

    empty Spring Boot application

---
+++ src/main/java/ru/animalcare/AnimalCareApplication.java
@@ -0,0 +1,12 @@
+package ru.animalcare;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AnimalCareApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(AnimalCareApplication.class, args);
+    }
+}
